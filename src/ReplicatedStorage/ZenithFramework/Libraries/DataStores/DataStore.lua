8c3923bab2316c6d7ffc93c9639b00df3873a825
Arthur Morton <a.smorton5@gmail.com>
2021-10-27 11:23:54 +0100

    DataStore Library updates

--- src/ReplicatedStorage/ZenithFramework/Libraries/DataStores/DataStore.lua
+++ src/ReplicatedStorage/ZenithFramework/Libraries/DataStores/DataStore.lua
@@ -2,6 +2,7 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
+local HttpService = game:GetService("HttpService")
 
 local require = table.unpack(require(ReplicatedStorage.ZenithFramework))
 
@@ -12,7 +13,7 @@ DataStore.Data = {}
 DataStore.DataCache = {}
 
 local AUTOSAVE_INTERVAL = 60
-local WAIT_INTERVAL = 30
+local DATA_WAIT_INTERVAL = 30
 
 -- Returns the stored session data, and creates a table if it doesn't exist yet
 function DataStore.getStoredData(dataStore)
@@ -46,12 +47,13 @@ function DataStore.getData(dataStore, index)
 
 	local data = DataStore.getStoredData(dataStore)
 	if data[index] then return data[index] end
-	local success, data = pcall(function()
+	local success, savedData, keyInfo = pcall(function()
 		dataStore:GetAsync(index)
 	end)
 	if success then 
-		data[index] = data
-		return data
+		print(keyInfo.Version, keyInfo.CreatedTime, keyInfo.UpdatedTime, keyInfo:GetUserIds(), keyInfo:GetMetadata())
+		data[index] = savedData
+		return savedData
 	else
 		warn("Failed to get data from DataStore: " , dataStore , " with index: " , index)
 	end
@@ -67,7 +69,7 @@ function DataStore.waitForData(dataStore, index)
 	while not data[index] do
 		DataStore.getData(dataStore, index)
 		if not data[index] then
-			task.wait(WAIT_INTERVAL)
+			task.wait(DATA_WAIT_INTERVAL)
 		end
 	end
 
@@ -75,14 +77,18 @@ function DataStore.waitForData(dataStore, index)
 end
 
 -- Completely overwrites the data in the given data store with the given index, saves it in the Data table and returns it
--- Also caches the data if it fails to save, to be picked up on the next autosave cycle
-function DataStore.setDataAsync(dataStore, index, newData)
+function DataStore.setDataAsync(dataStore, index, newData, userIds, metaData)
 	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
 	assert(typeof(index) == "string", "index argument must be a string")
 
-	local data = DataStore.getStoredData(dataStore)
+	local setOptions
+	if metaData then
+		setOptions= Instance.new("DataStoreSetOptions")
+		setOptions:SetMetadata(metaData)
+	end
+
 	local success, errorMessage = pcall(function()
-		data:SetAsync(index, newData)
+		dataStore:SetAsync(index, newData, userIds, setOptions)
 	end)
 	if not success then
 		warn("Failed to save data to DataStore: " , dataStore , " with index: " , index , " due to error: " , errorMessage)
@@ -91,11 +97,135 @@ function DataStore.setDataAsync(dataStore, index, newData)
 	end
 end
 
+-- Increments the number value in a data store
+function DataStore.incrementDataAsync(dataStore, index, newValue, userIds, metaData)
+	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
+	assert(typeof(index) == "string", "index argument must be a string")
+	assert(typeof(newValue) == "number", "newValue argument must be a number")
+
+	local incrementOptions 
+	if metaData then
+		incrementOptions= Instance.new("DataStoreIncrementOptions")
+		incrementOptions:SetMetadata(metaData)
+	end
+
+	local success, errorMessage = pcall(function()
+		dataStore:IncrementAsync(index, newValue, userIds, incrementOptions)
+	end)
+	if not success then
+		warn("Failed to increment data to DataStore: " , dataStore , " with index: " , index , " due to error: " , errorMessage)
+	else
+		DataStore.DataCache[dataStore][index] = nil
+	end
+end
+
+-- Updates the data using the given function
+function DataStore.updateDataAsync(dataStore, index, updateFunction)
+	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
+	assert(typeof(index) == "string", "index argument must be a string")
+	assert(typeof(updateFunction) == "function", "updateFunction argument must be a function")
+
+	local success, errorMessage = pcall(function()
+		return dataStore:UpdateAsync(index, updateFunction)
+	end)
+	if not success then
+		warn("Failed to increment data to DataStore: " , dataStore , " with index: " , index , " due to error: " , errorMessage)
+	else
+		DataStore.DataCache[dataStore][index] = nil
+	end
+end
+
+-- Restores a previous version of data, closest to the given date in form {year = 2020, month = 3, day = 09, etc...} (accepts minDate and maxData, where only maxDate is compulsory)
+function DataStore.restorePreviousVersion(dataStore, index, minDate, maxDate)
+	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
+	assert(typeof(index) == "string", "index argument must be a string")
+	assert(typeof(maxDate) == "table", "maxDate argument must be a table")
+
+	local minDateTime
+	if minDate and typeof(minDate) == "table" then
+		minDateTime = DateTime.fromUniversalTime(minDate.year or 0, minDate.month or 0, minDate.day or 0, minDate.hour or 0, minDate.min or 0)
+	end
+	local maxDateTime = DateTime.fromUniversalTime(maxDate.year, maxDate.month, maxDate.day, maxDate.hour or 0, maxDate.min or 0)
+	local listSuccess, pages = pcall(function()
+		return dataStore:ListVersionsAsync(index, Enum.SortDirection.Descending, minDateTime, maxDateTime.UnixTimestampMillis)
+	end)
+	if listSuccess then
+		local items = pages:GetCurrentPage()
+		if table.getn(items) > 0 then
+			-- Read the closest version
+			local closestEntry = items[1]
+			local success, value, info = pcall(function()
+				return dataStore:GetVersionAsync(index, closestEntry.Version)
+			end)
+			-- Restore current value by overwriting with the closest version
+			if success then
+				local userIds = info:GetUserIds()
+				local setOptions = Instance.new("DataStoreSetOptions")
+				setOptions:SetMetadata(info:GetMetadata())
+				DataStore.setDataAsync(dataStore, index, value, userIds, setOptions)
+			end
+		else
+			-- No entries found
+			warn("No entries found before given max date")
+		end
+	end
+end
+
+-- Attempts to list all the keys in a dataStore, and returns the pages table
+function DataStore.listKeys(dataStore)
+	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
+
+	local listSuccess, pages = pcall(function()
+		return dataStore:ListKeysAsync()
+	end)
+	if listSuccess then	
+		return pages
+	end
+end
+
+-- Returns the length of the JSON encoded data, used to check data limits
+function DataStore.getDataSize(dataStore, index)
+	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
+	assert(typeof(index) == "string", "index argument must be a string")
+
+	local data = DataStore.getStoredData(dataStore)
+	local success, encodedData = pcall(function()
+		local data = data[index] or DataStore.getData(dataStore, index)
+		if data then
+			return HttpService:JSONEncode(data)
+		else
+			error("Data doesn't exist")
+		end
+	end)
+	if success then
+		return string.len(encodedData)
+	end
+end
+
+-- Removes the index from a given dataStore object
+function DataStore.removeDataAsync(dataStore, index)
+	assert(typeof(dataStore) == "Instance", "DataStore argument must be a DataStore instance")
+	assert(typeof(index) == "string", "index argument must be a string")
+
+	local success, removedValue = pcall(function()
+		return dataStore:RemoveAsync(index)
+	end)
+	if success then
+		warn("Remove index: " .. index .. " from dataStore: " , dataStore , " successfully")
+	else
+		warn("Failed to remove index: " .. index .. " from dataStore: " , dataStore)
+	end
+end
+
 -- Saves all data in all datastores
 function DataStore.saveAllData()
 	for dataStore, data in pairs(DataStore.DataCache) do
 		for index, value in pairs(data) do
-			DataStore.setDataAsync(dataStore, index, value)
+			if typeof(value) == "number" then
+				DataStore.incrementDataAsync(dataStore, index, value)
+			else
+				DataStore.setDataAsync(dataStore, index, value)
+			end
 		end
 	end
 end
